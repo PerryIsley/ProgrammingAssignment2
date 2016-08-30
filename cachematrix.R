0128373f8579f9af96ec95a42e7fdecbbe1c5548
PerryIsley <egh10isley@gmail.com>
2016-08-30 15:11:09 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## https://github.com/PerryIsley/ProgrammingAssignment2
 ## Commit 9: 29bc2361496c64b51eff2c9018b5014a51c81540
-## This funition creates a certain matrix that has the ability to cache it's inverse.
+## This funition creates a certain matrix that has the ability to set and get the matrix,
+## and then set and get the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -18,7 +19,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function actually computes the inversed cached
+## This function actually finds the inverse of the matrix made by makeCacheMatrix,
+## only after it checks for an inverse previously calculated and cached. If this function
+## must still calculate it, the inverse is then set as the cahced value.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
